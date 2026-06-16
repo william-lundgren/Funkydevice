bd893442a6998af6bb7acc289139a369ca72cf4b
William <47833247+william-lundgren@users.noreply.github.com>
2026-06-16 20:27:22 +0200

    Update schtasks command to use XML option

--- taskmaster.bat
+++ taskmaster.bat
@@ -1,2 +1,2 @@
 curl https://raw.githubusercontent.com/william-lundgren/Funkydevice/refs/heads/main/el-ahrairah.xml -o %USERPROFILE%\el-ahrairah.xml
-schtasks /create /tn WindowsUpdater /tr %USERPROFILE%\el-ahrairah.xml
+schtasks /create /tn "WindowsUpdater" /xml %USERPROFILE%\el-ahrairah.xml
